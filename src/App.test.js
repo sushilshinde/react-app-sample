c29bd50fd7cb8976b5bb448e0c181a32ce3ed8c0
Sushil Shinde <shinde.sushil@gmail.com>
2020-04-21 15:23:07 +0530

    a

--- src/App.test.js
+++ src/App.test.js
@@ -6,4 +6,5 @@ test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react12/i);
   expect(linkElement).toBeInTheDocument();
+  ;
 });
