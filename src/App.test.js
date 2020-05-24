e11630dbb908f539a7d41ad3d7a010f4ca437027
Sushil Shinde <shinde.sushil@gmail.com>
2020-05-24 17:03:58 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react112/i);
+  const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
   ;
 });
